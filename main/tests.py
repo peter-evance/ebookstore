e04e13c6539153037593a74711de15c3de36992d
Peter Evance <peterevance1@gmail.com>
2021-06-20 22:44:49 +0300

    Homepage test added

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestPage(TestCase):
+    '''Test for http status code = 200 OK,
+    home.html template used and response containing name of the bookshop
+    '''
+    def test_home_page_works(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'ebookstore')
