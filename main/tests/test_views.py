265b23ba91ed448c277be5505e28fb894e789150
Peter Evance <peterevance1@gmail.com>
2021-06-30 08:30:56 +0300

    Added single book page

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,10 +1,11 @@
-from main import forms
+from main import forms, models
 from django.test import TestCase
 from django.urls import reverse
+from decimal import Decimal
 
 
 class TestPage(TestCase):
-    '''Test for http status code = 200 OK, if respective html template is used and 
+    '''Test for http status code = 200 OK, if respective html template is used and
     response containing name ebookstore.
     '''
 
@@ -29,5 +30,41 @@ class TestPage(TestCase):
         self.assertTemplateUsed(response, 'contact_us.html')
         self.assertContains(response, 'ebookstore')
         self.assertIsInstance(
-            response.context['form'], forms.ContactUsForm
-        )
+            response.context['form'], forms.ContactUsForm)
+
+    def test_products_page_returns_active(self):
+        models.Book.objects.create(
+            name="The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price=Decimal("10.00"),)
+        models.Product.objects.create(
+            name="A Tale of Two Cities",
+            slug="tale-two-cities",
+            price=Decimal("2.00"),
+            active=False,)
+        response = self.client.get(
+            reverse("books", kwargs={"tag": "all"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "ebookstore")
+        book_list = models.Book.objects.active().order_by("name")
+        self.assertEqual(
+            list(response.context["object_list"]), list(book_list))
+
+    def test_products_page_filters_by_tags_and_active(self):
+        cb = models.Book.objects.create(
+            name="The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price=Decimal("10.00"))
+        cb.tags.create(name="Open source", slug="opensource")
+        models.Book.objects.create(
+            name="Microsoft Windows guide",
+            slug="microsoft-windows-guide",
+            price=Decimal("12.00"))
+        response = self.client.get(
+            reverse("books", kwargs={"tag": "opensource"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "ebookstore")
+        book_list = (models.Book.objects.active().filter(
+            tags__slug="opensource").order_by("name"))
+        self.assertEqual(
+            list(response.context["object_list"]), list(book_list))
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from os import name
+from django.views.generic.detail import DetailView
 from django.urls import path
-from main import views
+from main import models, views
 
 
 urlpatterns = [
 
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
     path('books/<slug:tag>/', views.BookListView.as_view(), name='books'),
+    path('book/<slug:slug>/', DetailView.as_view(model=models.Book), name='book')
 
 ]
