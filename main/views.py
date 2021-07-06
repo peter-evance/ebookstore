5c7139c32389d8fd9cfad7c5d0383977f8c34cf0
Peter Evance <peterevance1@gmail.com>
2021-07-06 23:25:52 +0300

    Wired up sign up view function

--- main/views.py
+++ main/views.py
@@ -1,8 +1,40 @@
-# from django.http.response import HttpResponseRedirect
+import logging
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 from main import models, forms
+
+
+logger = logging.getLogger(__name__)
+
+
+class SignupView(FormView):
+    template_name = "signup.html"
+    form_class = forms.UserCreationForm
+    
+    def get_success_url(self):
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        
+        email = form.cleaned_data.get("email")
+        raw_password = form.cleaned_data.get("password1")
+        logger.info(
+            "New signup for email=%s through SignupView", email
+            )
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+        form.send_mail()
+        messages.info(
+            self.request, "You signed up successfully."
+            )
+        return response
+
 
 
 class ContactUsView(FormView):
@@ -13,16 +45,6 @@ class ContactUsView(FormView):
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
-
-# def contact_us(request):
-#     if request.method == 'POST':
-#         form = forms.ContactUsForm(request.POST)
-#         if form.is_valid():
-#             form.send_mail()
-#             return HttpResponseRedirect('/')
-#     else:
-#         form = forms.ContactUsForm()
-#     return render(request, 'contact_us.html', {'form': form})
 
 
 class BookListView(ListView):
