bce50ed5ef11ce95cd8ff2bb58b1ce7c70393734
Peter Evance <peterevance1@gmail.com>
2021-07-21 10:12:41 +0300

    Authentication/Login form added

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UsernameField
 from django.core.mail import send_mail
 from .models import User
-
+from django.contrib.auth import authenticate
 
 
 logger = logging.getLogger(__name__)
@@ -29,6 +29,39 @@ class UserCreationForm(DjangoUserCreationForm):
         fail_silently=True,
         )
         
+
+class AuthenticationForm(forms.Form):
+    email = forms.EmailField
+    password = forms.CharField(
+        strip=False, widget=forms.PasswordInput
+    )
+
+    def __init_(self, request=None,*args,**kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+        
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        
+        if email is not None and password:
+            self.user = authenticate(
+                self.request, email=email, password=password  
+            )
+            if self.user is None:
+                raise forms.ValidationError(
+                    "Incorrect email or password combination."
+                )
+            
+            logger.info(
+                "Login successful for email= %s", email
+            )
+            
+        return self.cleaned_data
+    
+    def get_user(self):
+        return self.user
 
 
 class ContactUsForm(forms.Form):
