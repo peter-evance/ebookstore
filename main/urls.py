b18a6f512302b488b88ca864e9062ffe5408e7e8
Peter Evance <peterevance1@gmail.com>
2021-07-21 10:21:11 +0300

    Login url....

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,15 @@
-from os import name
 from django.views.generic.detail import DetailView
 from django.urls import path
-from main import models, views
+from main import models, views, forms
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
-
+    path('signup/', views.SignupView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm,), name="login"),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
     path('books/<slug:tag>/', views.BookListView.as_view(), name='books'),
     path('book/<slug:slug>/', DetailView.as_view(model=models.Book), name='book'),
-    path('signup/', views.SignupView.as_view(), name="signup",)
+    
 
 ]
