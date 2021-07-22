d69a6b0b37717f1aee6f6168f3e4024c1b57517f
Peter Evance <peterevance1@gmail.com>
2021-07-22 23:56:36 +0300

    Address model functionality

--- main/models.py
+++ main/models.py
@@ -95,3 +95,31 @@ class BookImage(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='book-images')
     thumbnail = models.ImageField(upload_to='book-thumbnails', null=True)
+
+
+class Address(models.Model):
+    SUPPORTED_COUNTIES =(
+        ('ksm', 'Kisumu'),
+        ('nrb', 'nairobi'),
+        ('mbm', 'Mombasa')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    address1 = models.CharField("Address line 1", max_length=60)
+    address2 = models.CharField("Address line 2", max_length=60, blank=True)
+    zip_code = models.CharField("ZIP / Postal code", max_length=12)
+    city = models.CharField(max_length=60)
+    country = models.CharField(max_length=3, choices=SUPPORTED_COUNTIES)
+    
+    
+    def __str__(self):
+        return ", ".join(
+            [
+                self.name,
+                self.address1,
+                self.address2,
+                self.zip_code,
+                self.city,
+                self.country,
+            ]
+            )
