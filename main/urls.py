1d16490e6f767fddee99884722c91a99b0fff7aa
Peter Evance <peterevance1@gmail.com>
2021-07-22 23:44:19 +0300

    Login functionality...

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name="signup"),
-    path('login/', LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm,), name="login"),
+    path('login/', LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm), name="login"),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
     path('books/<slug:tag>/', views.BookListView.as_view(), name='books'),
     path('book/<slug:slug>/', DetailView.as_view(model=models.Book), name='book'),
