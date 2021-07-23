a2465cd9b5d43dca9ad673ac49be173799d121b1
Peter Evance <peterevance1@gmail.com>
2021-07-23 10:34:03 +0300

    Address update and deletion...

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('signup/', views.SignupView.as_view(), name="signup"),
     path('login/', LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm), name="login"),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
+    path('address/', views.AddressListView.as_view(), name="address_list"),
+    path('address/create/', views.AddressCreateView.as_view(), name="address_create"),
+    path('address/<int:pk>/',views.AddressUpdateView.as_view(),name="address_update"),
+    path('address/<int:pk>/delete/',views.AddressDeleteView.as_view(),name="address_delete"),
     path('books/<slug:tag>/', views.BookListView.as_view(), name='books'),
     path('book/<slug:slug>/', DetailView.as_view(model=models.Book), name='book'),
     
--- main/views.py
+++ main/views.py
@@ -70,8 +70,10 @@ tag all is specified'''
 
 class AddressListView(LoginRequiredMixin, ListView):
     model = models.Address
+    
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+
 class AddressCreateView(LoginRequiredMixin, CreateView):
     model = models.Address
     fields = [
@@ -84,9 +86,35 @@ class AddressCreateView(LoginRequiredMixin, CreateView):
     ]
     
     success_url = reverse_lazy("address_list")
+    
     def form_valid(self, form):
         obj= form.save(commit=False)
         obj.user= self.request.user
         obj.save()
         return super().form_valid(form)
     
+    
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Address
+    
+    fields = [
+        "name",
+        "address1",
+        "address2",
+        "zip_code",
+        "city",
+        "county",
+        ]
+    
+    success_url = reverse_lazy("address_list")
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+    
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Address
+    
+    success_url = reverse_lazy("address_list")
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
