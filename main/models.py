8a3bf70c8779eddbd6b66e5d8f1efbba266e2f60
Peter Evance <peterevance1@gmail.com>
2021-07-23 16:15:01 +0300

    Basket functionality-middleware handling...

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class UserManager(BaseUserManager):
@@ -123,3 +124,26 @@ class Address(models.Model):
                 self.country,
             ]
             )
+        
+class Basket(models.Model):
+    OPEN = 10
+    SUBMITTED = 20
+    STATUSES = (
+        (OPEN, "Open"),
+        (SUBMITTED, "Submitted")
+        )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True
+        )
+    status = models.IntegerField(choices=STATUSES, default=OPEN)
+    
+    def is_empty(self):
+        return self.basketline_set.all().count() == 0
+    
+    def count(self):
+        return sum(i.quantity for i in self.basketline_set.all())
+    
+class BasketLine(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
