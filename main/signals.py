72be4273a6b0ddf7c85271f76ec2f3a0548f2bab
Peter Evance <peterevance1@gmail.com>
2021-07-25 17:29:33 +0300

    Managing baskets in sessions...

--- main/signals.py
+++ main/signals.py
@@ -4,7 +4,8 @@ from PIL import Image
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import BookImage
+from .models import BookImage,Basket
+from django.contrib.auth.signals import user_logged_in
 
 THUMBNAIL_SIZE = (300, 300)
 
@@ -26,3 +27,25 @@ def generate_thumbnail(sender, instance, ** kwargs):
     instance.thumbnail.save(instance.image.name, ContentFile(
         temp_thumb.read()), save=False)
     temp_thumb.close()
+
+@receiver(user_logged_in)
+def merge_baskets_if_found(sender, user, request,**kwargs):
+    anonymous_basket = getattr(request,"basket",None)
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(
+                user=user, status=Basket.OPEN)
+            for line in anonymous_basket.basketline_set.all():
+                line.basket = loggedin_basket
+                line.save()
+                anonymous_basket.delete()
+                request.basket = loggedin_basket
+                logger.info(
+                    "Merged basket to id %d", loggedin_basket.id
+                    )
+        except Basket.DoesNotExist:
+            anonymous_basket.user = user
+            anonymous_basket.save()
+            logger.info(
+                "Assigned user to basket id %d",anonymous_basket.id,
+                )
