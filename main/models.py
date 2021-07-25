71de8818d867a75ed5de067729ac3ebe79adef7d
Peter Evance <peterevance1@gmail.com>
2021-07-25 19:48:30 +0300

    Order model added...

--- main/models.py
+++ main/models.py
@@ -147,3 +147,45 @@ class BasketLine(models.Model):
     basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+    
+    
+class Order(models.Model):
+    NEW = 10
+    PAID = 20
+    DONE = 30
+    STATUSES = ((NEW, "New"), (PAID, "Paid"), (DONE, "Done"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUSES, default=NEW)
+    billing_name = models.CharField(max_length=60)
+    billing_address1 = models.CharField(max_length=60)
+    billing_address2 = models.CharField(max_length=60,blank=True)
+    billing_zip_code = models.CharField(max_length=12)
+    billing_city = models.CharField(max_length=60)
+    billing_county = models.CharField(max_length=3)
+    shipping_name = models.CharField(max_length=60)
+    shipping_address1 = models.CharField(max_length=60)
+    shipping_address2 = models.CharField(max_length=60, blank=True)
+    shipping_zip_code = models.CharField(max_length=12)
+    shipping_city = models.CharField(max_length=60)
+    shipping_county = models.CharField(max_length=3)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+class OrderLine(models.Model):
+    NEW = 10
+    PROCESSING = 20
+    SENT = 30
+    CANCELLED = 40
+    STATUSES = (
+        (NEW, "New"),
+        (PROCESSING, "Processing"),
+        (SENT, "Sent"),
+        (CANCELLED, "Cancelled"),
+        )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="lines"
+        )
+    book = models.ForeignKey(
+        Book, on_delete=models.PROTECT
+        )
+    status = models.IntegerField(choices=STATUSES, default=NEW)
