df4a4cb5e522724ba9aac2747d8c457209a26a04
Peter Evance <peterevance1@gmail.com>
2022-10-16 06:26:57 +0300

    New order views

--- main/views.py
+++ main/views.py
@@ -5,9 +5,13 @@ from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteVi
 from django.views.generic.list import ListView
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse_lazy,reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponseRedirect
 from main import models, forms
+from django import forms as django_forms
+from django.db import models as django_models
+import django_filters
+from django_filters.views import FilterView
 
 
 logger = logging.getLogger(__name__)
@@ -33,7 +37,6 @@ class SignupView(FormView):
         form.send_mail()
         messages.info(self.request, "You signed up successfully.")
         return response
-
 
 
 class ContactUsView(FormView):
@@ -67,12 +70,14 @@ class BookListView(ListView):
             books = models.Book.objects.active()
         return books.order_by("name")
 
+
 class AddressListView(LoginRequiredMixin, ListView):
     template_name = "address_select.html"
     model = models.Address
     
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+
 
 class AddressCreateView(LoginRequiredMixin, CreateView):
     model = models.Address
@@ -85,6 +90,7 @@ class AddressCreateView(LoginRequiredMixin, CreateView):
         obj.user= self.request.user
         obj.save()
         return super().form_valid(form)
+
        
 class AddressUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Address
@@ -94,6 +100,7 @@ class AddressUpdateView(LoginRequiredMixin, UpdateView):
     
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+ 
     
 class AddressDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Address
@@ -151,3 +158,28 @@ class AddressSelectionView(LoginRequiredMixin, FormView):
             form.cleaned_data['billing_address'],
             form.cleaned_data['shipping_address'])
         return super().form_valid(form)
+    
+        
+class DateInput(django_forms.DateInput):
+    input_type = 'date'
+    
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = models.Order
+        fields = {
+                'user__email': ['icontains'],
+                'status': ['exact'],
+                'date_updated': ['gt', 'lt'],
+                'date_added': ['gt', 'lt'],
+                }
+        filter_overrides = {
+                django_models.DateTimeField: {
+                    'filter_class': django_filters.DateFilter,
+                    'extra': lambda f:{'widget': DateInput}}}
+
+class OrderView(UserPassesTestMixin, FilterView):
+    filterset_class = OrderFilter
+    login_url = reverse_lazy("login")
+    
+    def test_func(self):
+        return self.request.user.is_staff is True
