e2035850a7f9aa32c855b1bb51fd10f178bf66ad
Peter Evance <peterevance1@gmail.com>
2022-10-16 06:20:00 +0300

    Added new dependencies

--- ebookstore/settings.py
+++ ebookstore/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_extensions',
+    'debug_toolbar',
+    'django_tables2',
     'main.apps.MainConfig',
 ]
 
@@ -50,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = ['127.0.0.1']
+
 
 ROOT_URLCONF = 'ebookstore.urls'
 
