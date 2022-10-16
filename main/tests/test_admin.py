737e056da4e4492a475734939833930cad04cc9e
Peter Evance <peterevance1@gmail.com>
2022-10-16 14:48:45 +0300

    New admin view test trial code

--- main/tests/test_admin.py
+++ main/tests/test_admin.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 from main import factories
 from main import models
 
+from datetime import datetime
+from decimal import Decimal
+from unittest.mock import patch
+
 
 class TestAdminViews(TestCase):
     def test_most_bought_books(self):
@@ -32,3 +36,37 @@ class TestAdminViews(TestCase):
         print(response.context["labels"])
         print(response.context["values"])
         self.assertEqual(data,  {"Book 2": 3, "Book 3": 2, "Book 1": 5})
+        
+    # def test_invoice_renders_exactly_as_expected(self):
+    #     books = [
+    #         factories.BookFactory(name="Book 1", active=True, price=Decimal("100.00")),
+    #         factories.BookFactory(name="Book 2", active=True, price=Decimal("120.00")),
+    #         ]
+    #     user1 = models.User.objects.create_superuser("anonymous@gmail.com", "Abcd_123")
+    #     self.client.force_login(user1)
+    #     with patch("django.utils.timezone.now") as mock_time_now:
+    #         mock_time_now.return_value = datetime(
+    #             2022, 10, 16, 13, 59, 59)
+    #         order = factories.OrderFactory(
+    #             id=99,
+    #             user=user1,
+    #             billing_name="Peter Evance",
+    #             billing_address="Odyssey gates",
+    #             billing_town="Odin Halls",
+    #             billing_county="Odin",
+    #             )
+    #     factories.OrderLineFactory.create_batch(2, order=order, book=books[0])
+    #     factories.OrderLineFactory.create_batch(2, order=order, book=books[1])
+    #     response = self.client.get(reverse("admin:invoice", kwargs={"order_id": order.id}))
+    #     self.assertEqual(response.status_code, 200)
+    #     content = response.content.decode("utf8")
+    #     with open( "main/fixtures/invoice_test_order.html", "r") as fixture:
+    #         expected_content = fixture.read()
+    #     self.assertEqual(content, expected_content)
+    #     response = self.client.get(reverse("admin:invoice", kwargs={"order_id": order.id} ),{"format": "pdf"})
+    #     self.assertEqual(response.status_code, 200)
+    #     content = response.content
+        
+    #     with open( "main/fixtures/invoice_test_order.pdf", "rb") as fixture:
+    #         expected_content = fixture.read()
+    #     self.assertEqual(content, expected_content)
