8008ee4a86af3b7b8e88ac4c3fbbf19b67197c8e
Peter Evance <peterevance1@gmail.com>
2022-10-16 11:42:45 +0300

    New factory classes

--- main/factories.py
+++ main/factories.py
@@ -7,13 +7,19 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.User
 
-
 class BookFactory(factory.django.DjangoModelFactory):
     price = Decimal("80")
     
     class Meta:
         model = models.Book
+        
+class OrderLineFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.OrderLine
 
+class OrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Order
 
 class AddressFactory(factory.django.DjangoModelFactory): 
     class Meta:
